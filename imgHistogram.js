68c524b9347594a87627ddde9e6bc69f7a2d90c6
Artur Sum <sum.artur@gmail.com>
2017-02-06 17:42:55 +0100

    xAxis done

--- imgHistogram.js
+++ imgHistogram.js
@@ -1,14 +1,16 @@
 ((global)=>{
 
 
-    var createCanvasNode = (width, height)=>{
+    var createCanvasNode = (width, height, visible = false)=>{
 
       var node = document.createElement('canvas');
 
       node.width = width;
       node.height = height;
 
-      node.style.display = 'none';
+      if(!visible){
+          node.style.display = 'none';
+      }
 
       return node;
     },
@@ -34,7 +36,153 @@
         }
 
         return colorsMap;
+    },
+
+
+    findMaximumVal = (matrix)=>{
+
+            var currentMax = 0,
+                maximumColorVal = [];
+
+            for(let colorStorage of Object.values(matrix)){
+
+                colorStorage.forEach((val)=>{
+
+                    if(val > currentMax){
+                        currentMax = val;
+                    }
+
+                });
+
+                maximumColorVal.push(currentMax);
+                currentMax = 0;
+            }
+
+
+            return maximumColorVal;
+    },
+
+
+    setParams = (user, def)=>{
+
+
+        return {
+
+            height : user.height && typeof user.height == 'number'? Math.abs(Math.floor(user.height)) : def.height,
+            width : user.width && typeof user.width == 'number'? Math.abs(Math.floor(user.width)) : def.width
+
+        }
+
+
+    },
+
+
+    drawXAxis = (nodeOut, marginSize, requiredDataInterval)=>{
+
+        var ctx = nodeOut.getContext('2d'),
+            axisLength = nodeOut.width - (2*marginSize),
+            lineLength = 5;
+
+
+        //draw axis
+        ctx.save();
+        ctx.translate(marginSize, nodeOut.height - marginSize);
+
+        ctx.moveTo(0, 0);
+        ctx.lineTo(axisLength, 0);
+
+        ctx.stroke();
+
+
+        //#################################
+
+
+        var lineQnt = Math.round(255/requiredDataInterval),
+            intervalWidth = Math.round(axisLength/lineQnt);
+
+
+        for(var i=0; i<lineQnt; i++){
+
+            ctx.moveTo(i*intervalWidth, lineLength);
+            ctx.lineTo(i*intervalWidth, 0);
+            ctx.stroke();
+
+            ctx.fillText(`${i*requiredDataInterval}`, (i*intervalWidth)-8, 20);
+            ctx.stroke();
+
+        }
+
+        //last one label
+        ctx.moveTo(axisLength-1, lineLength);
+        ctx.lineTo(axisLength-1, 0);
+        ctx.stroke();
+
+        ctx.fillText(`255`, axisLength-7, 20);
+        ctx.stroke();
+
+        ctx.restore();
+
+    },
+
+    drawYAxis = (nodeOut, marginSize, maxVal)=>{
+
+        var ctx = nodeOut.getContext('2d'),
+            axisLength = nodeOut.height - (2*marginSize),
+            lineLength = 5;
+
+
+
+        ctx.save();
+        ctx.translate(marginSize, marginSize + axisLength);
+
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, -axisLength);
+        ctx.stroke();
+
+
+        var lineQnt = 10,
+            lineDrawLength = axisLength/lineQnt;
+
+
+
+        for(var i=0; i<lineQnt; i++){
+
+            ctx.moveTo(-lineLength, -i*lineDrawLength);
+            ctx.lineTo(0, -i*lineDrawLength);
+            ctx.stroke();
+
+            // ctx.fillText(`10000`, -20, (i*lineDrawLength)+4);
+            // ctx.stroke();
+
+        }
+
+        //last
+        ctx.moveTo(-lineLength, -axisLength+1);
+        ctx.lineTo(0, -axisLength+1);
+        ctx.stroke();
+
+
     };
+
+
+
+
+    //#########################################
+
+    var defaultConfig = {
+
+        height : 400,
+        width : 600
+
+
+
+    };
+
+
+
+
+
+
 
 
 
@@ -42,13 +190,22 @@
 
 
 
-    var imgHistogram = (conf={})=>{
+    var imgHistogram = (conf=defaultConfig)=>{
 
 
         var inImg = new Image(),
             canvasInNode, ctxIn,
             canvasOutNode, ctxOut,
-            colorsMap;
+            colorsMap, maxRgbVal,
+
+
+            params = setParams(conf, defaultConfig),
+            marginSize = 40;
+
+
+
+
+
 
 
         return{
@@ -81,7 +238,20 @@
                         ctxIn.getImageData(0, 0, this.width, this.height).data
                     );
 
+                    maxRgbVal = Math.max(...findMaximumVal(colorsMap));
+
                     console.log('color map: ', colorsMap);
+                    console.log(maxRgbVal);
+
+                    canvasOutNode = createCanvasNode(params.width, params.height, true);
+                    document.body.appendChild(canvasOutNode);
+                    ctxOut = canvasOutNode.getContext('2d');
+
+
+
+                    drawXAxis(canvasOutNode, marginSize, 16);
+                    drawYAxis(canvasOutNode, marginSize, maxRgbVal);
+
 
 
 
